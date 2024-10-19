650a10cf52c80d199089893ca4b37daab6c1f23b
tomhoyo <tom.hoyo@viacesi.fr>
2024-10-20 01:26:20 +0200

    add raycast to know if the player is grounded

---
+++ Assets/Scripts/Movement/TouchingDirections.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Movement
+{
+    internal class TouchingDirections : MonoBehaviour
+    {
+
+        private RaycastHit raycastHit;
+        private float groundDistance = 1.1f;
+
+        [SerializeField]
+        private bool _isGrounded;
+        public bool IsGrounded { get { return _isGrounded; } private set { _isGrounded = value; } }
+
+        private void FixedUpdate()
+        {
+
+            Debug.DrawRay(transform.position, Vector3.down * groundDistance, Color.red);
+
+            IsGrounded = Physics.Raycast(transform.position, Vector3.down, out raycastHit, groundDistance);
+        }
+
+    }
+}
